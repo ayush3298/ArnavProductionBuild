9ddcbbda114c8381e8196e1241d621988f8faa62
Ayush Bairagi <abairagi311@gmail.com>
2020-05-22 03:34:31 +0530

    added src

---
+++ assets/js/custom.js
@@ -0,0 +1,13 @@
+
+$('#popup4').on('shown.bs.modal', function (e) {
+
+  $('body').addClass('modal-open2');
+})
+
+$('#popup4').on('hide.bs.modal', function (e) {
+
+  $('body').removeClass('modal-open2');
+})
+      $(document).ready(function () {
+  $(".toast").toast("show");
+});
